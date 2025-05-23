8b77ef86bc3c05e6de857b24ec54a287751b1507
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2025-05-23 19:05:02 +0100

    more numbers

--- variables.cpp
+++ variables.cpp
@@ -3,9 +3,12 @@
 int main() {
     int x; //declaration
     x = 5; //assignment
+    //efficient way: x=5
+    int y= 6;
 
-    std::cout << x;
-
+    int sum = x+y;
+    std::cout << x << '\n' << y <<'\n' ;
+    std::cout << "Sum of x+y = "<<sum;
 
     return 0;
 }
