f8a8f27475c258f1008903d3ce39f53d3da644e9
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2025-05-23 19:22:18 +0100

    char

---
+++ moreVariables.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main() {
+
+    int age = 21;
+    int year = 2025;
+    int days = 7;
+
+    double number = 1.5;
+
+    //std::cout << number;
+
+    //char data type (Single Character)
+
+    char grade = 'A';
+    char intial = 'B';
+
+    //boolean
+
+    
+
+    std::cout << grade;
+
+
+
+    return 0;
+}
